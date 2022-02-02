5ad73dcd74b48ffe3394619fa3902fb270537cd9
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-02 18:12:45 +0200

    Add functionality for adding new item to the Db

--- 01. SMS_Skeleton-5.0 (2)/SMS/Controllers/ProductsController.cs
+++ 01. SMS_Skeleton-5.0 (2)/SMS/Controllers/ProductsController.cs
@@ -1,12 +1,20 @@
 ﻿using MyWebServer.Controllers;
 using MyWebServer.Http;
+using SMS.Data;
 using SMS.Models;
 using SMS.Validator;
+using SMS.Data.Models;
 
 namespace SMS.Controllers
 {
     public class ProductsController : Controller
     {
+        private SMSDbContext dbContext;
+
+        public ProductsController(SMSDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         [HttpGet]
         public HttpResponse Create()
         {
@@ -27,7 +35,17 @@ namespace SMS.Controllers
 
             if (ItemDataValidator.IsValidItemData(formModel.Name, formModel.Price))
             {
+                Data.Models.Product product = new Data.Models.Product()
+                {
+                    Name = formModel.Name,
+                    Price = formModel.Price,
+                };
+                this.dbContext
+                    .Products
+                    .Add(product);
 
+                this.dbContext
+                    .SaveChanges();
             }
 
             return this.Redirect("/Index");
