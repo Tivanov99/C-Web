290feb04bd4c69bcacc5d27e08c79785d45db838
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-02 22:44:32 +0200

    Add Carts Controller and some methods to him

---
+++ 01. SMS_Skeleton-5.0 (2)/SMS/Controllers/CartsController.cs
@@ -0,0 +1,33 @@
+﻿namespace SMS.Controllers
+{
+    using MyWebServer.Controllers;
+    using MyWebServer.Http;
+    using SMS.Data;
+    using System.Linq;
+
+    public class CartsController : Controller
+    {
+        private SMSDbContext dbContext;
+        public CartsController(SMSDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public HttpResponse AddProduct()
+        {
+            var result = this.Request.Query;
+            return this.View();
+        }
+
+        public HttpResponse Details()
+        {
+            if (this.User.IsAuthenticated)
+            {
+                //TODO : Get all data for current user cart
+                return this.View();
+            }
+            return this.Unauthorized();
+        }
+    }
+}
--- 01. SMS_Skeleton-5.0 (2)/SMS/Controllers/ProductsController.cs
+++ 01. SMS_Skeleton-5.0 (2)/SMS/Controllers/ProductsController.cs
@@ -1,13 +1,11 @@
-﻿using MyWebServer.Controllers;
-using MyWebServer.Http;
-using SMS.Data;
-using SMS.Models;
-using SMS.Validator;
-using SMS.Data.Models;
-using System.Linq;
-
-namespace SMS.Controllers
+﻿namespace SMS.Controllers
 {
+    using MyWebServer.Controllers;
+    using MyWebServer.Http;
+    using SMS.Data;
+    using SMS.Models;
+    using SMS.Validator;
+
     public class ProductsController : Controller
     {
         private SMSDbContext dbContext;
@@ -36,12 +34,11 @@ namespace SMS.Controllers
 
             if (ItemDataValidator.IsValidItemData(formModel.Name, formModel.Price))
             {
-
                 Data.Models.Product product = new Data.Models.Product()
                 {
                     Name = formModel.Name,
                     Price = formModel.Price,
-                    Cart= null
+                    Cart = null
                 };
                 this.dbContext
                     .Products
