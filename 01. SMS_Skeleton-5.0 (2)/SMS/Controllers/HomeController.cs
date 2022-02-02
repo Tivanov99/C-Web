ca71589260d05b0a7935342fcc24852c7b535da3
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-02 20:48:28 +0200

    Add all products to index page only for logged in users

--- 01. SMS_Skeleton-5.0 (2)/SMS/Controllers/HomeController.cs
+++ 01. SMS_Skeleton-5.0 (2)/SMS/Controllers/HomeController.cs
@@ -2,13 +2,32 @@
 {
     using MyWebServer.Controllers;
     using MyWebServer.Http;
+    using SMS.Data;
+    using SMS.Models;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class HomeController : Controller
     {
+        private SMSDbContext dbContext;
+
+        public HomeController(SMSDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         public HttpResponse Index()
         {
             if (this.User.IsAuthenticated)
             {
+                List<Product> products = this.dbContext
+                    .Products
+                    .Select(x => new Product()
+                    {
+                        Name = x.Name,
+                        Price = x.Price,
+                    })
+                    .ToList();
                 return this.View("IndexLoggedIn");
             }
             return this.View();
