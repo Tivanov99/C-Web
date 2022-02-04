433e4880cb79a12bbec5407ed1fe7b5f1cd0d848
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-04 13:32:42 +0200

    Add functionality to he UserDataValidator

---
+++ 01. Shared Trip_Skeleton - New Framework/MyWebServer/Common/GlobalConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace MyWebServer.Common
+{
+    public static class GlobalConstants
+    {
+        public const int UsernameMaxLength = 20;
+        public const int UsernameMinLength = 5;
+
+        public const int PasswordMaxLength = 20;
+        public const int PasswordMinLength = 6;
+
+        public const int SeatsMinValue = 2;
+        public const int SeatsMaxValue = 6;
+
+        public const int TripDescriptionMaxLength = 80;
+
+    }
+}
--- 01. Shared Trip_Skeleton - New Framework/SharedTrip/Validator/UserDataValidator.cs
+++ 01. Shared Trip_Skeleton - New Framework/SharedTrip/Validator/UserDataValidator.cs
@@ -1,21 +1,69 @@
-﻿namespace SharedTrip.Validator
+﻿using MyWebServer.Common;
+using System.Text.RegularExpressions;
+
+namespace SharedTrip.Validator
 {
     public class UserDataValidator
     {
+        private string emailRegexTemplate = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+
         public UserDataValidator()
         {
 
         }
 
-        public bool IsValidRegistraionData(string username,string email,
+        public bool IsValidRegistraionData(string username, string email,
             string password, string confirmPassword)
         {
-            return false;
+            if (!ValidUsername(username))
+            {
+                return false;
+            }
+
+            if (!ValidEmail(email))
+            {
+                return false;
+            }
+            if (!ValidPassword(password) ||
+                !ValidPassword(confirmPassword) ||
+                !PasswordComparator(password, confirmPassword))
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public bool IsValidLoginData(string username, string password)
         {
-            return false;
+            if (!ValidUsername(username))
+            {
+                return false;
+            }
+            if (!ValidPassword(password))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidUsername(string username)
+        {
+            return username.Length >= GlobalConstants.UsernameMinLength &&
+                username.Length <= GlobalConstants.UsernameMaxLength;
+        }
+        private bool ValidEmail(string email)
+        {
+            return Regex.IsMatch(email, this.emailRegexTemplate);
+        }
+        private bool ValidPassword(string password)
+        {
+            return password.Length >= GlobalConstants.PasswordMinLength &&
+                password.Length <= GlobalConstants.PasswordMaxLength;
+        }
+        private bool PasswordComparator(string password, string confirmPassword)
+        {
+            return password == confirmPassword;
         }
     }
 }
