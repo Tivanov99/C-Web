8c59a883a94a9eec4f2b56a53fb3bcff30a3401a
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-05 14:21:28 +0200

    Add functionality to TripService.cs (AddTrip) method

---
+++ 01. Shared Trip_Skeleton - New Framework/SharedTrip/AppServices/ITripService.cs
@@ -0,0 +1,16 @@
+﻿namespace SharedTrip.AppServices
+{
+    using SharedTrip.ApplicationModels;
+    using SharedTrip.Models;
+    using System.Collections.Generic;
+    public interface ITripService
+    {
+        IEnumerable<Trip> GetAllTrips();
+
+        bool AddTrip(CreatedTripForm createdTripForm);
+
+        Trip GetTrip(string id);
+
+        bool AddUserToTrip(string tripId, string userId);
+    }
+}
---
+++ 01. Shared Trip_Skeleton - New Framework/SharedTrip/AppServices/TripService.cs
@@ -0,0 +1,61 @@
+﻿namespace SharedTrip.AppServices
+{
+    using SharedTrip.ApplicationModels;
+    using SharedTrip.Data;
+    using SharedTrip.Models;
+    using SharedTrip.Validator;
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    public class TripService : ITripService
+    {
+        private ApplicationDbContext dbContext;
+        private TripDataValidator tripDataValidator;
+        public TripService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public bool AddTrip(CreatedTripForm createdTripForm)
+        {
+            if (this.tripDataValidator
+            .IsValidProduct(createdTripForm))
+            {
+                this.dbContext
+                    .Trips
+                    .Add(new Trip()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        StartPoint = createdTripForm.StartPoint,
+                        EndPoint = createdTripForm.EndPoint,
+                        DepartureTime = DateTime.ParseExact(createdTripForm.DepartureTime, "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture),
+                        Seats = createdTripForm.Seats,
+                        Description = createdTripForm.Description,
+                        ImagePath = createdTripForm.ImagePath
+                    });
+
+                this.dbContext
+                    .SaveChanges();
+
+                return true;
+            }
+            return false;
+        }
+
+        public bool AddUserToTrip(string tripId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Trip> GetAllTrips()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Trip GetTrip(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
