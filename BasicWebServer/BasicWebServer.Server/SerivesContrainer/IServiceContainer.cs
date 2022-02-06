fa0d838470852d375fd3ec9eef1f25b641095e6f
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-06 15:06:20 +0200

    Added IOC

---
+++ BasicWebServer/BasicWebServer.Server/SerivesContrainer/IServiceContainer.cs
@@ -0,0 +1,17 @@
+﻿namespace BasicWebServer.Server.SerivesContrainer
+{
+    public interface IServiceContainer
+    {
+        IServiceContainer Add<TService, TImplementation>()
+            where TService : class
+            where TImplementation : TService;
+
+        IServiceContainer Add<TService>()
+            where TService : class;
+
+        TService Get<TService>()
+            where TService : class;
+
+        object CreateInstance(Type serviceType);
+    }
+}
