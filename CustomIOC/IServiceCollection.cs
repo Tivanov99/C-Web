9efaef647f99bd9df5d078b405c88a575cacc0b2
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2022-02-06 14:36:13 +0200

    Add files via upload

---
+++ CustomIOC/IServiceCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustromInversionOfControll.Services
+{
+    public interface IServiceCollection
+    {
+        IServiceCollection Add<TService, TImplementation>()
+            where TService : class
+            where TImplementation : TService;
+
+        IServiceCollection Add<TService>()
+            where TService : class;
+
+        object CreateInstance(Type serviceType);
+
+        TService Get<TService>()
+            where TService : class;
+    }
+}
