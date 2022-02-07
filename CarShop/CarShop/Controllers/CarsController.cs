7c4816faeeb506b5d298edfdb94af87a704ceaae
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-07 23:00:07 +0200

    Added CarsController

---
+++ CarShop/CarShop/Controllers/CarsController.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace CarShop.Controllers
+{
+    using MyWebServer.Controllers;
+    using MyWebServer.Http;
+
+    public class CarsController : Controller
+    {
+        public HttpResponse All()
+        {
+            return this.View();
+        }
+    }
+}
