aaab55869da00d3fbbe9fa45bc85d5314b68a6aa
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-10 21:42:34 +0200

    Added Repository.cs

--- Git/Git/Data/Models/User.cs
+++ Git/Git/Data/Models/User.cs
@@ -1,15 +1,18 @@
 ﻿namespace Git.Data.Models
 {
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
     public class User
     {
         public string Id { get; set; }
 
+        [Required]
         public string Username { get; set; }
 
         public string Email { get; set; }
 
+        [Required]
         public string Password { get; set; }
 
         public List<Repository> Repositories { get; set; }
