37a0f82d4eb9a6875a618b290847d7d4d253dc75
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-10 21:36:36 +0200

    Added User data model

---
+++ Git/Git/Data/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Git.Data.Models
+{
+    using System.Collections.Generic;
+
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public List<Repository> Repositories { get; set; }
+
+        public List<Commit> Commits { get; set; }
+    }
+}
