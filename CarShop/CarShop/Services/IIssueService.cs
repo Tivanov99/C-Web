a59c44a431fafd0587007972a05a7d1011b0f4b6
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-10 16:01:50 +0200

    Added IIssueService and implement GetCarIssues to get all issues of car with passed Id as parameter

--- CarShop/CarShop/Services/IIssueService.cs
+++ CarShop/CarShop/Services/IIssueService.cs
@@ -1,7 +1,12 @@
-﻿namespace CarShop.Services
+﻿using CarShop.DTOS;
+using System.Collections.Generic;
+
+namespace CarShop.Services
 {
     public interface IIssueService
-    {   
+    {
+        List<IssueDTO> GetCarIssues(string carId);
 
+        void CreateCarIssue(string carId);
     }
 }
---
+++ CarShop/CarShop/Services/IssueService.cs
@@ -0,0 +1,36 @@
+﻿namespace CarShop.Services
+{
+    using CarShop.Data;
+    using CarShop.DTOS;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class IssueService : IIssueService
+    {
+        private ApplicationDbContext context;
+
+        public IssueService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void CreateCarIssue(string carId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<IssueDTO> GetCarIssues(string carId)
+        => this.context
+            .Issues
+            .AsNoTracking()
+            .Where(i => i.CarId == carId)
+            .Select(i => new IssueDTO()
+            {
+                Id = i.Id,
+                Description = i.Description,
+                IsFixed = i.IsFixed,
+            })
+            .ToList();
+    }
+}
