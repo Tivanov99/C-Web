bb586ce3aed761e978ca80d1f3c3c7b8f5526e64
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-10 21:54:52 +0200

    Created Repository.cs

---
+++ Git/Git/Data/Models/Commit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Data.Models
+{
+    public class Commit
+    {
+        public string Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        [ForeignKey(nameof(Creator))]
+        public string CreatorId { get; set; }
+
+        public User Creator { get; set; }
+
+        [ForeignKey(nameof(Repository))]
+        public string RepositoryId { get; set; }
+
+        public Repository Repository { get; set; }
+    }
+}
---
+++ Git/Git/Data/Models/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Data.Models
+{
+    public class Repository
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        [ForeignKey(nameof(Owner))]
+        public string OwnerId { get; set; }
+        public User Owner { get; set; }
+
+        public List<Commit> Commits { get; set; }
+    }
+}
