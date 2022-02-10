bc799d43418f940c366758f7b5991a3b7dae27f5
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-10 16:10:10 +0200

    Added ViewModels/AllViewsModel.cs plus injected issueService from constructor

--- CarShop/CarShop/Controllers/IssuesController.cs
+++ CarShop/CarShop/Controllers/IssuesController.cs
@@ -1,10 +1,18 @@
 ﻿namespace CarShop.Controllers
 {
+    using CarShop.Services;
     using MyWebServer.Controllers;
     using MyWebServer.Http;
 
     public class IssuesController : Controller
     {
+        private IssueService issueService;
+
+        public IssuesController(IssueService issueService)
+        {
+            this.issueService = issueService;
+        }
+
         public HttpResponse CarIssues(string carId)
         {
 
---
+++ CarShop/CarShop/ViewModels/AllViewsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CarShop.ViewModels
+{
+    using CarShop.DTOS;
+    using System.Collections.Generic;
+
+    public class AllViewsModel
+    {
+        public List<IssueDTO> Issues { get; set; }
+    }
+}
