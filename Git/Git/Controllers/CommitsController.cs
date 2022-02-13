429a5b1a456ee81c8b98554a6e2d6ee5672b3ddb
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-13 13:33:52 +0200

    Added CommitsController.cs

---
+++ Git/Git/Controllers/CommitsController.cs
@@ -0,0 +1,11 @@
+﻿namespace Git.Controllers
+{
+    using MyWebServer.Controllers;
+
+    public class CommitsController : Controller
+    {
+        public CommitsController()
+        {
+        }
+    }
+}
