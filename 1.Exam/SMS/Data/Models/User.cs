f0a08cb32425900a5b0074dcf0f63b07213af967
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-14 21:16:06 +0200

    Added User class for db model

---
+++ 1.Exam/SMS/Data/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Data.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(20), MinLength(5)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20), MinLength(6)]
+        public string Password { get; set; }
+
+        [ForeignKey(nameof(Cart))]
+        public string CartId { get; set; }
+
+        public Cart Cart { get; set; }
+    }
+}
