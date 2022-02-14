37e6bbcc953e8ff6c290185c8039edc7ce3bce56
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-14 21:19:57 +0200

    Added Product.cs to the db models

---
+++ 1.Exam/SMS/Data/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace SMS.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Product
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(20), MinLength(4)]
+        public string Name { get; set; }
+
+        [Range(0.05, 1000)]
+        public decimal Price { get; set; }
+
+        public Cart Cart { get; set; }
+
+        [ForeignKey(nameof(Cart))]
+        public string CartId { get; set; }
+    }
+}
