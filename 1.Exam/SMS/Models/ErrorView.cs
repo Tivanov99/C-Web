5c0c3c147830bf7bacfd4006f7c5824fc0abbaba
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-14 22:49:13 +0200

    Added ErrorView

---
+++ 1.Exam/SMS/Models/ErrorView.cs
@@ -0,0 +1,13 @@
+﻿namespace SMS.Models
+{
+    using System.Collections.Generic;
+
+    public class ErrorView
+    {
+        public ErrorView()
+        {
+            this.Errors = new();
+        }
+        public List<ErrorViewModel> Errors { get; set; }
+    }
+}
--- 1.Exam/SMS/Models/ErrorViewModel.cs
+++ 1.Exam/SMS/Models/ErrorViewModel.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SMS.Models
+﻿namespace SMS.Models
 {
     public class ErrorViewModel
     {
         public ErrorViewModel(string errorMessage)
         {
-            this.ErrorMessage = errorMessage;   
+            this.ErrorMessage = errorMessage;
         }
         public string ErrorMessage { get; set; }
     }
