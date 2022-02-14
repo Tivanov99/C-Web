c37c4d7c5c82349791cf0842cc00748d1dd82cb1
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-14 22:01:55 +0200

    Added IRepository.cs and Repository.cs to works with database

---
+++ 1.Exam/SMS/Data/Common/Repository.cs
@@ -0,0 +1,32 @@
+﻿namespace SMS.Data.Common
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.Linq;
+
+    public class Repository : IRepository
+    {
+        private readonly SMSDbContext dbContext;
+
+        public Repository(SMSDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            this.DbSet<T>().Add(entity);
+            this.SaveChanges();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+           => this.DbSet<T>().AsQueryable();
+
+        public int SaveChanges()
+        => this.dbContext.SaveChanges();
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return this.dbContext.Set<T>();
+        }
+    }
+}
