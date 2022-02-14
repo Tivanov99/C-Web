4729c0ee526b8014282071973c70a17caa1a8ac2
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-14 21:36:00 +0200

    Added Carts to db models

---
+++ 1.Exam/SMS/Data/Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿namespace SMS.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Cart
+    {
+        public Cart()
+        {
+            this.Products = new();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        public User User { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
--- 1.Exam/SMS/StartUp.cs
+++ 1.Exam/SMS/StartUp.cs
@@ -5,16 +5,21 @@
     using MyWebServer;
     using MyWebServer.Controllers;
     using MyWebServer.Results.Views;
+    using SMS.Data;
 
     public class StartUp
     {
         public static async Task Main()
-            => await HttpServer
-                .WithRoutes(routes => routes
-                    .MapStaticFiles()
-                    .MapControllers())
-                .WithServices(services => services
-                    .Add<IViewEngine, CompilationViewEngine>())
-                .Start();
+        {
+            SMSDbContext sMSDbContext = new SMSDbContext();
+            sMSDbContext.Database.EnsureCreated();
+        }
+            //=> await HttpServer
+            //    .WithRoutes(routes => routes
+            //        .MapStaticFiles()
+            //        .MapControllers())
+            //    .WithServices(services => services
+            //        .Add<IViewEngine, CompilationViewEngine>())
+            //    .Start();
     }
 }
