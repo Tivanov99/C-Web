e6028e36148cbc1d73ba6f9f13d5d0cb79ee64ed
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-15 17:50:44 +0200

    Added ProductsController.cs which will works with all products and CreateProductModel.cs which will receive all data for new product.

---
+++ 1.Exam/SMS/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿namespace SMS.Controllers
+{
+    using MyWebServer.Controllers;
+    using MyWebServer.Http;
+
+    public class ProductsController : Controller
+    {
+        public HttpResponse Create()
+        {
+            if (this.User.IsAuthenticated)
+            {
+                return this.View();
+            }
+            return this.Redirect("/Users/Login");
+        }
+        public HttpResponse Create()
+        {
+
+        }
+    }
+}
---
+++ 1.Exam/SMS/Models/CreateProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SMS.Models
+{
+    public class CreateProductModel
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
