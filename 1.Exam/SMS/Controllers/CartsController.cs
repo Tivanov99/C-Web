eaf84c1664b4b003ff150fd368b81169a935452f
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-16 18:55:23 +0200

    Added CartProductViewModel.cs for cart products and CartService.cs

--- 1.Exam/SMS/Controllers/CartsController.cs
+++ 1.Exam/SMS/Controllers/CartsController.cs
@@ -1,6 +1,17 @@
-﻿namespace SMS.Controllers
+﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+
+namespace SMS.Controllers
 {
-    public class CartsController
+    public class CartsController : Controller
     {
+        public HttpResponse Details()
+        {
+            if (this.User.IsAuthenticated)
+            {
+                return this.View();
+            }
+            return this.Redirect("/Users/Login");
+        }
     }
 }
---
+++ 1.Exam/SMS/Services/CartService.cs
@@ -0,0 +1,27 @@
+﻿namespace SMS.Services
+{
+    using SMS.Data.Common;
+    using SMS.Data.Models;
+    using SMS.Models;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CartService
+    {
+        private readonly IRepository repo;
+        public CartService(IRepository repository)
+        {
+            this.repo = repository;
+        }
+
+        public List<CartProductViewModel> AllProducts()
+        => this.repo.All<Product>()
+                .Select(p => new CartProductViewModel()
+                {
+                    Name = p.Name,
+                    Price = p.Price,
+                })
+                .ToList();
+
+    }
+}
