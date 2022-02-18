4fc378a2b115c8d35234a277a65dce6a407d3fea
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-18 14:09:34 +0200

    Created CarService.cs and fully implementation

--- Exam2/CarShop/CarShop/Contracts/ICarService.cs
+++ Exam2/CarShop/CarShop/Contracts/ICarService.cs
@@ -1,9 +1,12 @@
-﻿namespace CarShop.Contracts
+﻿using CarShop.ViewModels;
+using System.Collections.Generic;
+
+namespace CarShop.Contracts
 {
     public interface ICarService
     {
-        (bool, string) Create();
+        (bool, string) Create(CarCreateViewModel carModel,string userId);
 
-
+        List<CarViewModel> AllCars(string userId);
     }
 }
---
+++ Exam2/CarShop/CarShop/Services/CarService.cs
@@ -0,0 +1,96 @@
+﻿using CarShop.Contracts;
+using CarShop.Data.Common;
+using CarShop.Data.DataModels;
+using CarShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarShop.Services
+{
+    public class CarService : ICarService
+    {
+        private readonly IRepository repo;
+        private readonly IValidationService validationService;
+
+        public CarService(
+            IRepository repo,
+            IValidationService validationService
+            )
+        {
+            this.repo = repo;
+            this.validationService = validationService;
+        }
+        public List<CarViewModel> AllCars(string userId)
+        {
+            bool isUserMechanic = GetUserAccessorType(userId);
+
+            if (!isUserMechanic)
+            {
+                return this.repo
+                    .All<Car>()
+                    .Where(c => c.OwnerId == userId)
+                    .Select(c => new CarViewModel()
+                    {
+                        PlateNumber = c.PlateNumber,
+                        FixedIssues = c.Issues.Where(i => i.IsFixed == true).Count(),
+                        RemainingIssues = c.Issues.Where(i => i.IsFixed == false).Count(),
+                    })
+                    .ToList();
+            }
+
+            return this.repo
+                .All<Car>()
+                .Where(c => c.Issues.Any(i => i.IsFixed == false))
+               .Select(c => new CarViewModel()
+               {
+                   PlateNumber = c.PlateNumber,
+                   FixedIssues = c.Issues.Where(i => i.IsFixed == true).Count(),
+                   RemainingIssues = c.Issues.Where(i => i.IsFixed == false).Count(),
+               })
+                    .ToList();
+        }
+
+
+        public (bool, string) Create(CarCreateViewModel carModel, string userId)
+        {
+            bool registered = false;
+            string error = null;
+
+            var (isValid, validationError) = validationService
+                .Validate(carModel);
+
+            if (!isValid)
+            {
+                return (isValid, validationError);
+            }
+
+            Car car = new Car()
+            {
+                Model = carModel.Model,
+                Year = carModel.Year,
+                PictureUrl = carModel.Image,
+                OwnerId = userId,
+            };
+
+            try
+            {
+                repo.Add(car);
+                repo.SaveChanges();
+                registered = true;
+            }
+            catch (Exception)
+            {
+                error = "Could not save user in DB";
+            }
+
+            return (registered, error);
+        }
+
+        private bool GetUserAccessorType(string userId)
+            => this.repo.All<User>()
+            .Where(u => u.Id == userId)
+            .Select(u => u.IsMechanic)
+            .FirstOrDefault();
+    }
+}
