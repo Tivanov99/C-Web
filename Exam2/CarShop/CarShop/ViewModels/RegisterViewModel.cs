30a5be3f528446b74523a5d9d930416c276829c2
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-18 11:44:29 +0200

    Refactored RegisterViewModel

--- Exam2/CarShop/CarShop/ViewModels/RegisterViewModel.cs
+++ Exam2/CarShop/CarShop/ViewModels/RegisterViewModel.cs
@@ -1,13 +1,19 @@
-﻿namespace CarShop.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarShop.ViewModels
 {
     public class RegisterViewModel
     {
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "{0} must be between {2} and {1} sybols.")]
         public string Username { get; set; }
 
+        [EmailAddress(ErrorMessage = "Email must be valid email")]
         public string Email { get; set; }
 
-        public string Passwords { get; set; }
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string Password { get; set; }
 
+        [Compare(nameof(Password), ErrorMessage = "Password and ConfirmPassword must be equal")]
         public string ConfirmPassword { get; set; }
 
         public string UserType { get; set; }
