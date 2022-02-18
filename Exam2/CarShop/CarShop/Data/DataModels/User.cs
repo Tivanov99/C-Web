27d242e27f3b2d880e0a2d58815c1472b71ac90d
Tivanov99 <trifon.ivanov21@gmail.com>
2022-02-18 10:59:23 +0200

    Created User data model

---
+++ Exam2/CarShop/CarShop/Data/DataModels/User.cs
@@ -0,0 +1,23 @@
+﻿namespace CarShop.Data.DataModels
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class User
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        //[StringLength(20, MinimumLength = 4, ErrorMessage = "Username must be between {2} and {1} sybols.")]
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [StringLength(20, MinimumLength = 5)]
+        [Required]
+        public string Password { get; set; }
+
+        public bool IsMechanic { get; set; }
+    }
+}
